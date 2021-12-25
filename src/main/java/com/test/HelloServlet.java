e262f901a586dcd422a90db84dbd766b9cdceb27
K <dkh1001@outlook.kr>
2021-12-25 21:54:44 +0900

    7. Servlet Life-Cycle

--- src/main/java/com/test/HelloServlet.java
+++ src/main/java/com/test/HelloServlet.java
@@ -3,52 +3,53 @@ package com.test;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloServlet
- */
 @WebServlet("/hs")
 public class HelloServlet extends HttpServlet {
+	//Servlet 실행 부분
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public HelloServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		PrintWriter out = response.getWriter();
-		out.print("<html>");
-		out.print("<head>");
-		out.print("</head>");
-		out.print("<body>");
-		out.print("<div>");
-		out.print("Hello Servlet");
-		out.print("</div>");
-		out.print("</body>");
-		out.print("</html>");
+		System.out.println("DoGet");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
+	@PostConstruct
+	public void postConstruct(){
+	  System.out.println("PostConstruct");
+	}
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("Init Method");
+	}
+
+	@Override
+	public void destroy() {
+	  super.destroy();
+	  System.out.println("Destroy Method");
+	}
+
+	@PreDestroy
+	public void preDestroy(){
+	  System.out.println("PreDestroy");
+	}
+	
 }
+
